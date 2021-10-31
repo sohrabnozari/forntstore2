e3dd2f3cbb35ef9dd9b4a2997ec497f62c44a881
sohrabnozari <sohrabnozari11@gmail.com>
2021-10-31 22:10:59 +0330

    complete views and seralizer files

--- store/urls.py
+++ store/urls.py
@@ -8,10 +8,10 @@ from rest_framework_nested import routers
 
 router = routers.DefaultRouter()
 router.register('products', views.ProductViewSet)
-router.register('collection', views.CollectionViewSet)
+router.register('collections', views.CollectionViewSet)
 
 product_router = routers.NestedDefaultRouter(
-    router, 'products')
+    router, 'products', lookup='product')
 product_router.register('reviews', views.ReviewViewSet,
                         basename='product-reviews')
 
--- store/views.py
+++ store/views.py
@@ -36,8 +36,14 @@ class CollectionViewSet(ModelViewSet):
             return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
         collection.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+# -------------------------------------------------------------------------------------------
 
 
 class ReviewViewSet(ModelViewSet):
-    queryset = Review.objects.all()
-    serializer_class=ReviewSerializer
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        return Review.objects.filter(product_id=self.kwargs['product_pk'])
+
+    def get_serializer_context(self):
+        return {'product_id': self.kwargs['product_pk']}
