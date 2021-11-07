2dd34dab466e73826b305ced8b4a536559fd453d
sohrabnozari <sohrabnozari11@gmail.com>
2021-11-07 20:36:41 +0330

    creating getting carts opreation but creating cart dosnt work properly

--- store/serializer.py
+++ store/serializer.py
@@ -1,6 +1,9 @@
 import collections
 from rest_framework import serializers
 from decimal import Decimal
+from rest_framework import fields
+
+from rest_framework.fields import SerializerMethodField
 from .models import Cart, CartItem, Collection, Product, Review
 
 
@@ -37,15 +40,33 @@ class ReviewSerializer(serializers.ModelSerializer):
         return Review.objects.create(product_id=product_id, **validated_data)
 
 
+class SimpleProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'inventory',
+                  'unit_price', 'collection']
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product = SimpleProductSerializer()
+    total_price = SerializerMethodField()
+
+    def get_total_price(self, cartitem=CartItem):
+        return cartitem.quantity * cartitem.product.unit_price
+
+    class Meta:
+        model = CartItem
+        fields = ['id', 'product', 'quantity', 'total_price']
+
+
 class CartSerializer(serializers.ModelSerializer):
-    # id = serializers.UUIDField(read_only=True)
+    id = serializers.UUIDField(read_only=True)
+    items = CartItemSerializer(many=True)
+    total_price = SerializerMethodField()
+
+    def get_total_price(self, cart):
+        return sum([item.quantity*item.product.unit_price for item in cart.items.all()])
 
     class Meta:
         model = Cart
-        fields = ['id']
-
-
-# class CartItemSerializer(serializers.Serializer):
-#     class Meta:
-#         models: CartItem
-#         fields: ['cart', 'product', 'quantitiy']
+        fields = ['id', 'items', 'total_price']
--- store/views.py
+++ store/views.py
@@ -9,7 +9,7 @@ from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.filters import SearchFilter
-from rest_framework.mixins import CreateModelMixin
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin
 from .filters import ProductFilter
 from .serializer import CartSerializer, ProductSerializer, CollectionSerilizer, ReviewSerializer
 from .models import Product, Collection, Review, Cart
@@ -55,6 +55,6 @@ class ReviewViewSet(ModelViewSet):
 # ----------------------------------------------------------------------------
 
 
-class CartViewSet(CreateModelMixin, GenericViewSet):
-    queryset = Cart.objects.all()
+class CartViewSet(CreateModelMixin, RetrieveModelMixin, GenericViewSet):
+    queryset = Cart.objects.prefetch_related('items__product').all()
     serializer_class = CartSerializer
